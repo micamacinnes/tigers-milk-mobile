a9ee56d46aee56e526d956275424504fa88faf2f
sydneytu <sydneytu@usc.edu>
2018-06-11 21:45:27 +0200

    login integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,27 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { Http, HttpModule } from "@angular/http";
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { PaymentPage } from '../pages/payment/payment';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    PaymentPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    PaymentPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,52 @@
+
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, App } from 'ionic-angular';
+// import { LoginPage } from '../login/login';
+// import { RegisterPage } from '../registration/registration';
+import { Http } from "@angular/http";
+import { PaymentPage } from '../payment/payment';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public email: string;
+  public password: string;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private app: App,
 
-  }
+    public http: Http) { }
 
+    login() {
+      this.http
+        .post("http://localhost:3000/login", {
+          email: this.email,
+          password: this.password
+        })
+        .subscribe(
+          result => {
+            console.log(result);
+    
+            // Our username and password (on this) should have data from the user
+            this.navCtrl.push(PaymentPage, {
+              email: this.email,
+              password: this.password
+            });
+          },
+    
+          error => {
+            console.log(error);
+          }
+        );
+    }
 }
+
+// navigateTologin() {
+
+//   this.navCtrl.setRoot(LoginPage, {
+//     username: this.username
+//   });
+
+// }
