971efa17964f56daa42fa99d9c6c5541f37d48bb
micamac <micamac@umich.edu>
2018-06-12 11:44:07 +0200

    regsiter page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public loadingCtrl: LoadingController) {
 
   }
+  presentLoading() {
+    const loader = this.loadingCtrl.create({
+      content: "Please wait...",
+      duration: 3000
+    });
+    loader.present();
 
-}
+  }
+  navigateToRegister() {
+    this.navCtrl.push(RegisterPage);
+  }
+  }
+
+
